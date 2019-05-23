1c682fd9e87aad780bdaeb134106841652c30fc2
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-05-23 18:07:50 +0200

    Fridge controller

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,27 +1,43 @@
 class FridgesController < ApplicationController
   def index
+    @fridges = Fridge.all
   end
 
   def show
+    @fridge = Fridge.find(params[:id])
   end
 
   def new
+    @fridge = Fridge.new
   end
 
   def create
+    @fridge = Fridge.new(fridge_params)
+    if @fridge.save
+      redirect_to fridge_path(@fridge)
+    else
+      render :new
   end
 
   def edit
+    @fridge = Fridge.find(params[:id])
   end
 
   def update
+    @fridge = Fridge.find(params[:id])
+    @fridge.update(fridge_params)
+    redirect_to fridges_path
   end
 
   def destroy
+    @fridge = Fridge.find(params[:id])
+    @fridge.destroy
+    redirect_to fridges_path
   end
 
   private
 
   def fridge_params
+    params.require(:fridge).permit(:name, :address, :brand, :availability, :description, :photo, :capacity, :price)
   end
 end
