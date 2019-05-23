68a6e81e55ca3705f7a64b08120972e80b8e0b9b
tom halimi <tomcompteperso@gmail.com>
2019-05-23 18:41:46 +0200

    I did a MTF controller for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,25 +3,41 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(params[:booking])
+    @booking.save
+
+    redirect_to dashboard_path
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(params[:booking])
+
+    redirect_to dashboard_path
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    redirect_to bookings_path
   end
 
   private
 
   def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
