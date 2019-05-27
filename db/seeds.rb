12d97744f89a91aeb27ac937eb3d0f1feeafa493
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-05-27 11:21:14 +0200

    lots of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Fridge.create(name: 'Bosch Serie 4 KDV29VL30', address: '1 rue Crespin du Gast', brand: 'Bosch',
+Fridge.create!(name: 'Bosch Serie 4 KDV29VL30', address: '1 rue Crespin du Gast', brand: 'Bosch',
   photo: "https://static.fnac-static.com/multimedia/Images/FR/MC/4e/75/2b/19625294/1507-1/tsp20170925083012/Bosch-Serie-4-KDV29VL30-refrigerateur-congelateur-congelateur-haut-pose-libre-aspect-inox.jpg#2563251e-e9b7-4168-8737-45d026eb6498",
-  capacity: '194', price: '514',
+  capacity: '194', price: '514', user_id: 1,
   description: "4 x étagère - verre de sécurité ¦ 3 x étagère - easyAccess ¦ Tiroir - contrôle de l'humidité ¦ 1 x étagère pour bouteilles - chrome ¦ 1 x Compartiment de porte - Largeur complète ¦ 2 x Compartiment de porte - demi-largeur ¦ Tiroir - CrisperBox")
-Fridge.create(name: 'Continental-edison Cef2d227b2', address: '2 rue Crespin du Gast', brand: 'Continental',
+Fridge.create!(name: 'Continental-edison Cef2d227b2', address: '2 rue Crespin du Gast', brand: 'Continental',
   photo: "https://static.fnac-static.com/multimedia/Images/6F/6F/37/55/5584751-3-1520-1/tsp20190426154240/Continental-edison-Cef2d227b2-Refrigerateur-Congelateur-Haut-227l-187-40-Froid-Statique-A-L-54cm-X-H-144cm-Noir.jpg",
-  capacity: '187', price: '283',
+  capacity: '187', price: '283', user_id: 1,
   description: "Réfrigérateur congélateur haut - Volume 227L (187L + 40L) - Froid statique - Classe énergétique A+ - Autonomie en cas de coupure : 14h - 3 clayettes en verre - Portes réversibles - Coloris : Noir")
-Fridge.create(name: 'Combine Inverse Electrolux En 3350', address: '3 rue Crespin du Gast', brand: 'Electrolux',
+Fridge.create!(name: 'Combine Inverse Electrolux En 3350', address: '3 rue Crespin du Gast', brand: 'Electrolux',
   photo: "https://static.fnac-static.com/multimedia/Images/4A/4A/4D/AE/11423050-1505-1505-1/tsp20190420005112/Combine-Inverse-Electrolux-En-3350-Mox.jpg#7611ff4a-7c0b-4c18-b565-c4a3548a8559",
-  capacity: '220', price: '516',
+  capacity: '220', price: '516', user_id: 1,
   description: "Réfrigérateur - Congélateur en bas Volume net congélateur (L) : 91 Volume net réfrigérateur (L) : 220 Volume total net (L) : 311 Zone 0°C / Compartiment fraîcheur : Non")
-Fridge.create(name: 'Hotpoint SH61QXRD', address: '4 rue Crespin du Gast', brand: 'Hotpoint',
+Fridge.create!(name: 'Hotpoint SH61QXRD', address: '4 rue Crespin du Gast', brand: 'Hotpoint',
   photo: "https://static.fnac-static.com/multimedia/Images/E6/E6/5B/A9/11099110-1505-1505-1/tsp20190213113526/Refrigerateur-Hotpoint-SH61QXRD.jpg#ae1c5aef-78d1-4794-902a-0a0621906151",
-  capacity: '321', price: '866',
+  capacity: '321', price: '866', user_id: 1,
   description: "Réfrigérateur - pose libre - largeur : 59.5 cm - profondeur : 64.5 cm - hauteur : 167 cm - 321 litres - classe A+ - acier inoxydable")
-Fridge.create(name: 'Beersafe Pro ', address: '5 rue Crespin du Gast', brand:'Klarstein',
+Fridge.create!(name: 'Beersafe Pro ', address: '5 rue Crespin du Gast', brand:'Klarstein',
   photo: "https://static.fnac-static.com/multimedia/Images/5B/5B/E0/B3/11788379-1505-1505-1/tsp20190514041544/Klarstein-Beersafe-Pro-Refrigerateur-a-boions-133-litres-0-a-22-C-porte-vitree-eclairage-LED-Noir.jpg#33b39187-72c6-438e-8e4c-23309d83bf1a",
-  capacity: '133', price: '519',
+  capacity: '133', price: '519', user_id: 1,
   description: "Bien froid pour mieux se rafraîchir : le Klarstein Beersafe Pro est un réfrigérateur de bar compact d’une capacité de 133 litres pour le stockage de boissons rafraîchissantes.")
-Fridge.create(name: 'Panasonic NR-BN31EX1', address: '6 rue Crespin du Gast', brand: 'Panasonic',
+Fridge.create!(name: 'Panasonic NR-BN31EX1', address: '6 rue Crespin du Gast', brand: 'Panasonic',
   photo: "https://static.fnac-static.com/multimedia/Images/FR/MC/38/0b/c0/29363000/1520-2/tsp20161021170931/Panasonic-NR-BN31EX1-refrigerateur-congelateur-congelateur-bas-pose-libre-inox.jpg",
-  capacity: '223', price: '1726',
+  capacity: '223', price: '1726', user_id: 1,
   description: "Le réfrigérateur NR­BN31EX1 de Panasonic préserve la fraîcheur de vos aliments plus longtemps comme jamais auparavant. Les solutions novatrices de stockage et de préservation de la fraîcheur comme la membrane BreatheTech, la technologie VitaminSafe LED de préservation des vitamines, le système FlexLift, Slide'n'Store et le bac MaxiBox permettent de conserver les nutriments, les vitamines et les minéraux de vos aliments.")
-Fridge.create(name: 'LG Gsl360icev', address: '7 rue Crespin du Gast', brand: 'LG',
+Fridge.create!(name: 'LG Gsl360icev', address: '7 rue Crespin du Gast', brand: 'LG',
   photo: "https://static.fnac-static.com/multimedia/Images/7F/7F/5F/55/5595007-1505-1505-1/tsp20190516205441/Lg-Gsl360icev-Refrigerateur-601-L.jpg#367e915d-8123-475f-adfa-bb57c5cfc11a",
-  capacity: '591', price: '1148',
+  capacity: '591', price: '1148', user_id: 1,
   description: "591 Volume net réfrigérateur (L)    394 Volume net congélateur (L)    197 Froid ventilé total (Total NoFrost)    Oui Autonomie en cas de panne (h)    10 Classe climatique    T (+18 à +43°C) Niveau sonore (dB)")
-Fridge.create(name: 'Samsung RF56J9040SR', address: '8 rue Crespin du Gast', brand: 'Samsung',
+Fridge.create!(name: 'Samsung RF56J9040SR', address: '8 rue Crespin du Gast', brand: 'Samsung',
   photo: "https://static.fnac-static.com/multimedia/Images/E5/E5/B4/53/5485797-3-1520-2/tsp20190320233647/SAMSUNG-RF56J9040SR-Refrigerateur-americain-564L-361-203-L-Froid-ventile-A-L-90-8-x-H-182-5-cm.jpg",
-  capacity: '564', price: '1999',
+  capacity: '564', price: '1999', user_id: 1,
   description: "4 x étagère - verre trempé résistant aux liquides ¦ 1 x étagère - rétractable ¦ 1 x étagère - retractable ¦ 2 x tiroir - contrôle de l'humidité")
-Fridge.create(name: 'Whirlpool ART6614/A+SF', address: '9 rue Crespin du Gast', brand: 'Whirlpool',
+Fridge.create!(name: 'Whirlpool ART6614/A+SF', address: '9 rue Crespin du Gast', brand: 'Whirlpool',
   photo: "https://image.darty.com/encastrable/refrigerateur-congel_encastrable/refrigerateur_congelateur_bas_encastrable/whirlpool_art6614_a_sf_l1610184270380A_162201826.jpg",
-  capacity: '275', price: '549',
+  capacity: '275', price: '549', user_id: 1,
   description: "Cet appareil peut fonctionner dans une température ambiante allant de +10°C à +43°C. Idéal dans les habitats où la température s'élève beaucoup en été ou dans une cuisine où le réfrigérateur-congélateur se situe face à une fenêtre plein sud ou près d'une surface chaude !")
 
